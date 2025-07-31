10b86b8be4330edf1701bcb877a775f47c23cb36
Tomas Kojar <thomaskojar@gmail.com>
2025-07-31 19:21:37 -0400

    Add new function to demonstrate Git workflow

--- test_example.py
+++ test_example.py
@@ -4,5 +4,9 @@ print("Hello from test file!")
 def test_function():
     return "This is a test function"
 
+def new_function():
+    return "This is a new function added to demonstrate Git workflow"
+
 if __name__ == "__main__":
-    print(test_function()) 
+    print(test_function())
+    print(new_function()) 
