50ed8fe517d66b629dca39c11b32dc403b6af04d
Tomas Kojar <thomaskojar@gmail.com>
2025-07-31 19:20:34 -0400

    Add test example file to demonstrate Git workflow

---
+++ test_example.py
@@ -0,0 +1,8 @@
+# This is a test file to demonstrate Git workflow
+print("Hello from test file!")
+
+def test_function():
+    return "This is a test function"
+
+if __name__ == "__main__":
+    print(test_function()) 
